05fe39bcc6ef57414aa700b3c8052ad65407eab2
Kelvin Owens <147459916+KelvinOwens1@users.noreply.github.com>
2024-03-15 12:08:02 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Header from './components/header/Header'
 import Menu from './components/menu/Menu'
 import Footer from './components/footer/Footer'
 
+
 function App() {
   return (
     <div className="container">
       <Header />
-      {/* <Menu />
-      <Footer /> */}
+      <Menu />
+      <Footer />
     </div>
   )
 }
